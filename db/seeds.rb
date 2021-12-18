490c7eabacb5efcc215323f7263b4b6deb924e8b
Luke Hayes <lukepurplehayes@gmail.com>
2021-12-18 10:26:18 +0000

    Added random color generation seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+def create_color
+  hex_values = %w(a b c d e f 1 2 3 4 5 6 7 8 9)
+  s = ""
+  1.upto(6) do |x|
+    s += hex_values.sample
+  end
+
+  return s
+end
+
+1.upto(10) do |c|
+  color = Color.new
+  new_color = create_color()
+  color.value = "#" + new_color.to_s
+  color.save
+end
